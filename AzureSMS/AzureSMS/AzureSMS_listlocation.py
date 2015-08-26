9c98e7feaaa8ba69f5ce0ee0d1332c8bdaf14a8f
dafoyiming <dafoyiming@gmail.com>
2015-08-26 13:44:01 +0800

    更新listlocation

---
+++ AzureSMS/AzureSMS/AzureSMS_listlocation.py
@@ -0,0 +1,13 @@
+﻿from azure import *
+from azure.servicemanagement import *
+
+subscription_id = '4c1f7e7c-e47c-4688-8de0-19b1f8b58636'
+certificate_path = 'C:\cer\zymcert.pem'
+
+sms = ServiceManagementService(subscription_id,
+                               certificate_path,
+                               'management.core.chinacloudapi.cn')
+
+result = sms.list_locations()
+for location in result:
+    print(location.name)
