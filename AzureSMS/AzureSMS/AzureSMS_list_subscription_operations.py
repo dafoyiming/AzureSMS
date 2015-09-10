d95a274da49e317fdf41482d6cd84a60af6b7dea
dafoyiming <dafoyiming@gmail.com>
2015-09-10 16:35:43 +0800

    添加交互输入

--- AzureSMS/AzureSMS/AzureSMS_list_subscription_operations.py
+++ AzureSMS/AzureSMS/AzureSMS_list_subscription_operations.py
@@ -1,22 +1,36 @@
-﻿from azure import *
+﻿import sys
+from azure import *
 from azure.servicemanagement import *
 
-subscription_id = '4c1f7e7c-e47c-4688-8de0-19b1f8b58636'
-certificate_path = 'C:\Azure_Cert\zymcert-home.pem'
+if __name__ == '__main__':
 
-sms = ServiceManagementService(subscription_id,
-                               certificate_path,
-                               'management.core.chinacloudapi.cn')
-
-
-operations = sms.list_subscription_operations('2015-09-01T16:30:00Z','2015-09-09T16:30:00Z',operation_result_filter = 'Failed')
+    subscription_id = raw_input("Please input Subscription ID:")
+    certificate_path = raw_input("Please input your certification location:")
+    start_time = raw_input("Please input the start time [eg:2015-09-01T16:30:00Z]:")
+    end_time = raw_input("Please input the start time [eg:2015-09-01T16:30:00Z]:")
+    filter = raw_input("Please input the operation status[Succeeded, Failed, or InProgress enter = all]:")
 
 
-for i in range(0,len(operations.subscription_operations)):
+#subscription_id = '4c1f7e7c-e47c-4688-8de0-19b1f8b58636'
+#certificate_path = 'C:\Azure_Cert\zymcert-home.pem'
 
-    print('NO:' + str(i) )
-    print('operation_id: ' + operations.subscription_operations[i].operation_id)
-    print('operation_name: ' + operations.subscription_operations[i].operation_name)
-    print('operation_started_time: ' + operations.subscription_operations[i].operation_started_time)
-    print('operation_status: ' + operations.subscription_operations[i].operation_status.status)
-    print('==========================================')
+    sms = ServiceManagementService(subscription_id,
+                                   certificate_path,
+                                   'management.core.chinacloudapi.cn')
+
+
+
+    try:
+        operations = sms.list_subscription_operations(start_time,end_time,operation_result_filter = filter)
+    except Exception as e :
+        print "error：" + str(e)
+
+
+    for i in range(0,len(operations.subscription_operations)):
+
+        print('NO:' + str(i) )
+        print('operation_id: ' + operations.subscription_operations[i].operation_id)
+        print('operation_name: ' + operations.subscription_operations[i].operation_name)
+        print('operation_started_time: ' + operations.subscription_operations[i].operation_started_time)
+        print('operation_status: ' + operations.subscription_operations[i].operation_status.status)
+        print('==========================================')
