e1cbce617b33a7b008c4d2f829e7900a598e6eaf
dafoyiming <dafoyiming@gmail.com>
2015-09-10 14:35:44 +0800

    add_list_operations

--- AzureSMS/AzureSMS/AzureSMS_list_subscription_operations.py
+++ AzureSMS/AzureSMS/AzureSMS_list_subscription_operations.py
@@ -1,14 +1,22 @@
-from azure import *
+﻿from azure import *
 from azure.servicemanagement import *
 
 subscription_id = '4c1f7e7c-e47c-4688-8de0-19b1f8b58636'
-certificate_path = 'C:\cer\zymcert.pem'
+certificate_path = 'C:\Azure_Cert\zymcert-home.pem'
 
 sms = ServiceManagementService(subscription_id,
                                certificate_path,
                                'management.core.chinacloudapi.cn')
 
-operations = sms.list_subscription_operations('2015-09-08T16:30:00Z','2015-09-09T16:30:00Z')
 
-operations
+operations = sms.list_subscription_operations('2015-09-01T16:30:00Z','2015-09-09T16:30:00Z',operation_result_filter = 'Failed')
 
+
+for i in range(0,len(operations.subscription_operations)):
+
+    print('NO:' + str(i) )
+    print('operation_id: ' + operations.subscription_operations[i].operation_id)
+    print('operation_name: ' + operations.subscription_operations[i].operation_name)
+    print('operation_started_time: ' + operations.subscription_operations[i].operation_started_time)
+    print('operation_status: ' + operations.subscription_operations[i].operation_status.status)
+    print('==========================================')
